f2c5b0c893584ee162f09645a5124d6f64d4b544
maSta89 <hastikj@seznam.cz>
2021-04-09 16:02:31 +0200

    Add project files.

---
+++ CustomerAPI/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAPI {
+    public class Customer {
+        public int customerID { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public Customer() {
+        }
+
+    }
+}
